f1be1f2b69c6fb62e95f7d51b7d213864f3f8846
farf1991 <taha@perf-on.com>
2026-03-29 05:18:45 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
-
-  const token = req.cookies.get('sb-access-token')?.value ||
-    req.cookies.get(`sb-${process.env.NEXT_PUBLIC_SUPABASE_URL?.split('//')[1]?.split('.')[0]}-auth-token`)?.value
-
-  const isPublic = pathname.startsWith('/login') || pathname === '/'
-
-  if (isPublic && !token) return NextResponse.next()
-  if (!token && !isPublic) return NextResponse.redirect(new URL('/login', req.url))
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/login', '/admin/:path*', '/membre/:path*']
+  matcher: []
 }
